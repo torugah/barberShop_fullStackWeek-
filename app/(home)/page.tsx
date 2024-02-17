83b1aec634f14c07770da489a5a5f8e52722996f
torugah <fillipibarros@gmail.com>
2024-02-17 16:36:39 -0300

    feat: add inicial search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,20 +2,25 @@ import { format } from "date-fns"
 import Image from "next/image";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale/pt-BR";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
     <div>
       <Header/>
 
-    <div className="px-5 pt-5">
-    <h2 className="text-xl font-bold">Olá, Fillipi!</h2>
-      <p className="capitalize text-sm"> 
-        {format(new Date(), "EEEE',' dd 'de' MMMM", {
-          locale: ptBR
-        })}
-      </p>
-    </div>
+      <div className="px-5 pt-5">
+      <h2 className="text-xl font-bold">Olá, Fillipi!</h2>
+        <p className="capitalize text-sm"> 
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR
+          })}
+        </p>
+      </div>
+
+      <div className="px-5 mt-6">
+        <Search />
+      </div>
     </div>
   )
 }

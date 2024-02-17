315083cdabd7a51bb030b473ba903e38be5631f7
torugah <fillipibarros@gmail.com>
2024-02-17 16:26:53 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,21 @@
+import { format } from "date-fns"
 import Image from "next/image";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale/pt-BR";
 
 export default function Home() {
   return (
     <div>
       <Header/>
+
+    <div className="px-5 pt-5">
+    <h2 className="text-xl font-bold">Olá, Fillipi!</h2>
+      <p className="capitalize text-sm"> 
+        {format(new Date(), "EEEE',' dd 'de' MMMM", {
+          locale: ptBR
+        })}
+      </p>
+    </div>
     </div>
   )
 }
